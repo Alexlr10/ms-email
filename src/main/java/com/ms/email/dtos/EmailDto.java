b4c6da1adf40c91f38f7c18fc32eba0391b0d50a
Alexlr10 <alexlr10@hotmail.com>
2023-04-13 11:56:35 -0300

    first commit

---
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,30 @@
+package com.ms.email.dtos;
+
+import com.ms.email.models.EmailModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+@Data
+public class EmailDto {
+
+    @NotBlank
+    private String ownerRef;
+    @NotBlank
+    @Email
+    private String emailFrom;
+    @NotBlank
+    @Email
+    private String emailTo;
+    @NotBlank
+    private String subject;
+    @NotBlank
+    private String text;
+
+    public EmailModel convertToEmailModel(){
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(this, emailModel);
+        return emailModel;
+    }
+}
